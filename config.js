9a5a4f5869b5d2f686f0f92a2fbaa9924ce0380c
HOMO-BOY <103184953+HOMO-BOY@users.noreply.github.com>
2022-04-07 18:22:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'WCT博客',
-  githubName: 'china-wct',
+  title: 'HOMO-BOY博客',
+  githubName: 'HOMO-BOY',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://wctbk.vercel.app',
-  SEO_description: 'WCT博客，写我所想',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,WCT博客，自由的博客',
+  SEO_description: 'HOMO-BOY',
+  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,HOMO-BOY',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
